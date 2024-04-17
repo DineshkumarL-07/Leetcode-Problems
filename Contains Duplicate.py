e3372492022308efddb9dfc33ecfba6aefe5d3cd
dinesh <ldineshkumar38@gmail.com>
2024-04-17 21:17:53 +0530

    Updated the Contains Duplicate.py file

--- Contains Duplicate.py
+++ Contains Duplicate.py
@@ -1,1 +1,19 @@
-hashSet = set()
+# Input: nums = [1,2,3,1]
+# Output: true
+
+# Input: nums = [1,2,3,4]
+# Output: false
+
+# Code
+
+def contains_duplicate(nums):
+    hashSet = set()
+    for num in nums:
+        if num in hashSet:
+            return True
+        hashSet.add(num)
+    return False
+
+nums = [1,2,3,1]
+print(contains_duplicate(nums))
+
