c1a48ef0cc75bac9eb9cf9eee4aaf83e29a7e45f
dinesh <ldineshkumar38@gmail.com>
2024-04-19 20:25:27 +0530

    Modified Valid_anagram.py

--- Valid_anagram.py
+++ Valid_anagram.py
@@ -1,1 +1,19 @@
-import itertools
+# Input: s = "anagram", t = "nagaram"
+# Output: true
+
+# Input: s = "rat", t = "car"
+# Output: false
+
+def is_anagram(s,t):
+    if len(s) != len(t):
+        return False
+    hashMap1, hashMap2 = {}, {}
+    for i in range(len(s)):
+        hashMap1[s[i]] = 1 + hashMap1.get(s[i],0)
+        hashMap2[t[i]] = 1 + hashMap2.get(t[i],0)
+    
+    return hashMap1 == hashMap2
+
+s = "anagram"
+t = "nagaram"
+print(is_anagram(s,t))
